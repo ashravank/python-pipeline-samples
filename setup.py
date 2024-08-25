1140b04ae43d17e79b8028bc36f15e49696cee37
ashravank <bi.shravan20@gmail.com>
2024-08-25 10:57:58 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-pipeline-samples',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'click==8.1.3',
+        'Flask==2.2.2',
+        'itsdangerous==2.1.2',
+        'Jinja2==3.1.2',
+        'MarkupSafe==2.1.1',
+        'Werkzeug==2.2.2',
+    ],
+)
