bc83b51441ff972babebd64235095e01eb1b9874
ashravank <bi.shravan20@gmail.com>
2024-08-25 10:42:28 -0700

    Create setup.py

---
+++ .harness/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Flask>=2.0.0',
+        'requests>=2.25.0',
+          ],
+)
